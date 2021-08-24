46b85b32511c2cb6792b021cf09845f0113cd148
voiiden <voldenissen@gmail.com>
2021-08-24 20:03:50 +0200

    Multiple improvements Declutter code, implement link preview, better variable names

--- bg.js
+++ bg.js
@@ -8,63 +8,51 @@
 chrome.omnibox.onInputChanged.addListener(
   function (text, suggest) {
     console.log('inputChanged: ' + text);
-    // suggest([
-    //   {content: ">http://www.autistici.org/0xFE/!bang/", description: "goto !bang web site"},
-    //   {content: ">http://www.autistici.org/0xFE/!bang/man.php", description: "goto !bang online documentation"}
-    // ]);
-  });
+    var results = text.split(" "); // ["yt", "wintergatan"]
+    var bang = (results.shift()); // first element of array
+    var stringquery = results.join("%20");
+    var queryURL = make_queryURL(take_searchengine(bang), stringquery);
+    suggest([{ content: ">" + queryURL, description: queryURL }]);
 
-//chrome.omnibox.onInputStarted.addListener(
-//	function(){
-//		console.log('inputStarted: ');
-//    	//add web site sugestion
-//	}
-//);
-// function readBangs(json) {
-//   var arr = 
-// }
+  }
+);
+
 // configuration ...
-var searchsigil = '!';
 var defaultengine = 'g';
-var gosigil = '>';
+var linkstart = ">"
 
 // Load bangs from json
 const bangs_json_path = chrome.runtime.getURL('bangs.json')
 fetch(bangs_json_path)
   .then(response => response.json())
-  .then(data => {arr = data});
-
-
-// chrome.runtime.onStartup.addListener(
-//   console.log("Online")
-// );
+  .then(data => { banglist = data });
 
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function (text) {
+    if (text.charAt(0) == linkstart) { queryURL = text.slice(1); } // Keep all characters except first (0 index)
+    else {
+      var results = text.split(" "); // ["yt", "wintergatan"]
+      var bang = (results.shift()); // return and remove first element of array
+      var stringquery = results.join("%20");
+      var queryURL = make_queryURL(take_searchengine(bang), stringquery);
+    }
 
-    var results = text.split(" "); // ["yt", "wintergatan"]
-    console.log(results) 
-    var bang = ("!" + results.shift()); // first element of array
-    // console.log(searchengine)
-    if (bang.charAt(0) != searchsigil) { results.unshift(bang); bang = "null"; }
-    var stringquery = results.join("%20");
-    var queryURL = make_queryURL(take_searchengine(bang), stringquery);
-    //alert(queryURL);
     navigate(queryURL);
-  });
-function take_searchengine(ss) {
-  var ret = undefined;
-  var localelang = window.navigator.language;
+  }
+);
 
-  var engin = ss.substr(1);
-  ret = arr[engin];
-  if (!ret) { ret = arr[defaultengine]; }
-  return ret;
+function take_searchengine(bang) {
+  var baseURL = undefined;
+  // var localelang = window.navigator.language;
+  baseURL = banglist[bang];
+  if (!baseURL) { baseURL = banglist[defaultengine]; }
+  return baseURL;
 };
-function make_queryURL(searchengine, stringquery) {
-  var ret = searchengine.replace('%s', stringquery);
-  return ret;
+
+function make_queryURL(baseURL, stringquery) {
+  var queryURL = baseURL.replace('%s', stringquery);
+  return queryURL;
 };
 
 function navigate(url) {
